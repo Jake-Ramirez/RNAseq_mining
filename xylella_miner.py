2fb2087ff0123a9bfbca014441b72c067bd17d9c
Jacobo Y Dobon - AUB <josepjacobo@gmail.com>
2026-04-12 13:34:26 -0500

    creating the main script xylella_miner.py and adding description

---
+++ xylella_miner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''
+XyllellaMiner: Pathogen detection in vector RNA-seq data
+Specialized for Homalodisca vitripennis - Xylella fastidiosa system
+
+Author: Jacobo Dobon
+Date: April 2026
+'''
