03de0bf5829445a986bad5af6f84e67d37bc01ca
Jacobo Y Dobon - AUB <josepjacobo@gmail.com>
2026-04-12 19:47:19 -0500

    script to extract the unmapped reads to the host, bacteria potentially

---
+++ preprocess_homalodisca.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# preprocess_homalodisca.sh: Prepare Homalodisca RNA-seq data for Xylella detection
+# 
+# This script filters out host reads and prepares clean reads for pathogen detection.
+
+set -e  # Exit if something goes south
+
